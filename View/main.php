257a83b57da1549adef3eb7259131d82bccf67cf
0NZ1T4M <velislav.boyanov@gmail.com>
2020-07-13 08:10:09 +0300

    Fixed bad indent on html files.

--- View/main.php
+++ View/main.php
@@ -10,25 +10,24 @@
 <body>
 
 <h2> Enter field parameters.</h2>
-<form action="index.php?target=field&action=add" method="post">
-                <div class="form-group">
-                    <label>Width(Max 8):</label>
-                    <input type="text" name = "Width" class="form-control" required>
-                <div class="form-group">
-                    <label>Length(Max 8):</label>
-                    <input type="text" name = "Length" class="form-control" required>
-                <div class="form-group">
-                    <label>End X:</label>
-                    <input type="text" name = "End_X" class="form-control" required>
-                <div class="form-group">
-                    <label>End Y:</label>
-                    <input type="text" name = "End_Y" class="form-control" required>
-                <div class="form-group">
-                    <label>Bombs(Max < 1):</label>
-                    <input type="text" name = "Bomb_Intensity" class="form-control" required>
-                    <button type="submit" class="btn btn-primary"> Start </button>
-                </div>
-            </form>
-
+    <form action="index.php?target=field&action=add" method="post">
+        <div class="form-group">
+            <label>Width(Max 8):</label>
+            <input type="text" name = "Width" class="form-control" required>
+        <div class="form-group">
+            <label>Length(Max 8):</label>
+            <input type="text" name = "Length" class="form-control" required>
+        <div class="form-group">
+            <label>End X:</label>
+            <input type="text" name = "End_X" class="form-control" required>
+        <div class="form-group">
+            <label>End Y:</label>
+            <input type="text" name = "End_Y" class="form-control" required>
+        <div class="form-group">
+            <label>Bombs(Max < 1):</label>
+            <input type="text" name = "Bomb_Intensity" class="form-control" required>
+            <button type="submit" class="btn btn-primary"> Start </button>
+        </div>
+    </form>
 </body>
 </html>
